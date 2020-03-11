0e7f2beda39a0090f17f9dd2dc3238c3c0d2cc5f
dshine <dshine@gofree.indigo.ie>
2020-03-11 16:59:39 +0100

    First verson of the game

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Game");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0,0,forwardForce * Time.deltaTime);
+        Debug.Log("Updating");
+    }
+}
